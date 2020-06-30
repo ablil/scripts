2bc4233973209cda745e9b72386b62535709a386
ablil <ablil@protonmail.com>
2020-06-30 10:56:38 +0100

    add: sort directory content by creation time

---
+++ shell/sort_by_creation_time.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+###########################################################
+# Script Name	: sort_by_creation_time.sh
+# Description	: Sort files by creation time and not modification time
+# Arguments	: Directory containing files/directories
+# Date		: 2020-06-20
+# Author	: _ablil
+# Email		: ablil@protonmail.com
+###########################################################
+
+$directory=$1
+[[ -z $1 ]] && echo -e "Usage:\t bash $0 <directory>" && exit;
+
+format_ouput() {
+# echo creation date followed by filename
+    for file in $(find $directory -type f); do
+        created_at=$(stat $file | tail -1 | cut -d ' ' -f3,4)
+        filename=$file
+        echo "$created_at $(basename $filename)"
+    done;
+}
+
+format_ouput | sort
